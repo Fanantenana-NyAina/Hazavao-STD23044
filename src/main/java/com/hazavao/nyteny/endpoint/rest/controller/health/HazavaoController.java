eedf7f629b1b3e8564342e665dad8caf24080722
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-06-13 09:20:18 +0300

    feat: HazavaoController

---
+++ src/main/java/com/hazavao/nyteny/endpoint/rest/controller/health/HazavaoController.java
@@ -0,0 +1,30 @@
+package com.hazavao.nyteny.endpoint.rest.controller.health;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HazavaoController {
+
+    private final String apiKey = System.getenv("OPENAI_API_KEY");
+
+    @GetMapping("/hazavao")
+    public String getDefinition(@RequestParam String teny) {
+        OpenAiService service = new OpenAiService(apiKey);
+
+        CompletionRequest request = CompletionRequest.builder()
+                .model("gpt-3.5-turbo")
+                .prompt("Dikateny ny '" + teny + "' : ")
+                .maxTokens(50)
+                .build();
+
+        return service.createCompletion(request)
+                .getChoices()
+                .getFirst()
+                .getText();
+    }
+
+}
